6596d07c643efe04d630edef665e24c8b6c63cef
Ashikunnabi <ashikunnabituhin@gmail.com>
2019-09-03 08:10:57 +0600

    template distributed

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from .views import IndexView, ProductListView, ProductDetailView
+
+
 urlpatterns = [
-    
+    path('', IndexView.as_view(), name='index'),
+    path('category/', ProductListView.as_view(), name='product_list'),
+    path('details/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'product/index.html'
+    
+    def get_context_data(self, **kwargs):    
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a Title of response page
+        context['title'] = 'Home'
+        return context
+        
+# change the view as list view        
+class ProductListView(TemplateView):
+    template_name = 'product/category.html'
+    
+    def get_context_data(self, **kwargs):    
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a Title of response page
+        context['title'] = 'CategoryName'
+        return context
+        
+# change the view as detail view        
+class ProductDetailView(TemplateView):
+    template_name = 'product/single-product.html'
+    
+    def get_context_data(self, **kwargs):    
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a Title of response page
+        context['title'] = 'ProductName'
+        return context
